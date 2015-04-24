f9e18f52717ce7783b6f0743ecfe8170f6f8b2cb
RireMakar <abellavia42@gmail.com>
2015-04-24 13:16:35 -0400

    qr
    AASDDAS

---
+++ FloorIsLava/FloorIsLava/Gold.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FloorIsLava
+{
+    /*
+     * The Gold class will inherit from the GameObject class and will add to the players 
+     * gold attribute if it is collided with.
+     */
+    class Gold
+    {
+    }
+}
