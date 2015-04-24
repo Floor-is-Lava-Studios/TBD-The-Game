1cf00087dd8f7076f1d8c4769bf3915f7618562a
JacobRoos <bopzor@gmail.com>
2015-04-24 14:50:53 -0400

    Make the Map Maker Better
    Now select the piece to place and press space to add it to the board.
    Can see the piece to place.
    Better loading in images.
    Better drawing images.

--- MapCreator/MapCreator/MapImages.cs
+++ MapCreator/MapCreator/MapImages.cs
@@ -9,18 +9,29 @@ namespace MapCreator
     public static class MapImages
     {
         // constants
-        public static const string WallImage = "wall.PNG";
-        public static const string PlayerImage = "player.PNG";
-        public static const string GoalImage = "goal.PNG";
-        public static const string GoldImage = "gold.PNG";
-        public static const string EnemyImage = "enemy.PNG";
-        public static const string EnemyUpImage = "enemyUp.PNG";
-        public static const string EnemyRightImage = "enemyRight.PNG";
-        public static const string EmptyImage = "empty.PNG";
-        public static const string CursorImage = "cursor.PNG";
-        public static const string ObstacleImage = "obstacle.PNG";
+        public const string WallImage = "wall.PNG";
+        public const string PlayerImage = "player.PNG";
+        public const string GoalImage = "goal.PNG";
+        public const string GoldImage = "gold.PNG";
+        public const string EnemyImage = "enemy.PNG";
+        public const string EnemyUpImage = "enemyUp.PNG";
+        public const string EnemyRightImage = "enemyRight.PNG";
+        public const string EmptyImage = "empty.PNG";
+        public const string CursorImage = "cursor.PNG";
+        public const string ObstacleImage = "obstacle.PNG";
 
-        public static const int ImageHeight = 100;
-        public static const int ImageWidth = 100;
+        public const string WallHalfImage = "wallHalf.PNG";
+        public const string PlayerHalfImage = "playerHalf.PNG";
+        public const string GoalHalfImage = "goalHalf.PNG";
+        public const string GoldHalfImage = "goldHalf.PNG";
+        public const string EnemyHalfImage = "enemyHalf.PNG";
+        public const string EnemyUpHalfImage = "enemyUpHalf.PNG";
+        public const string EnemyRightHalfImage = "enemyRightHalf.PNG";
+        public const string EmptyHalfImage = "emptyHalf.PNG";
+        public const string CursorHalfImage = "cursorHalf.PNG";
+        public const string ObstacleHalfImage = "obstacleHalf.PNG";
+
+        public const int ImageHeight = 100;
+        public const int ImageWidth = 100;
     }
 }
