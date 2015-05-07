704c13aef1c860f2ef024cbeb731baab73081365
Josh9309 <joshmalmquist96@gmail.com>
2015-05-06 23:27:46 -0400

    Got All Level info stuff done and added some options
    Made the Reset method for the unlocked level file. which will reset all
    levels but the first one to locked. add three options to option screen.
    only the reset level lock works so far. all the levels should transition
    smoothly. Goal Still isn't working.

--- FloorIsLava/FloorIsLava/OptionScreen.cs
+++ FloorIsLava/FloorIsLava/OptionScreen.cs
@@ -19,7 +19,8 @@ namespace FloorIsLava
         private Game1 game;
         private KeyboardState lastState;
         private GameState gameState;
-
+        private int count;
+        private SaveInfo info;
         private Texture2D back;
         #endregion Attributes
 
@@ -31,6 +32,8 @@ namespace FloorIsLava
             gameState = new GameState(game); // creates new gamestate object and assigns it to gameState
             font1 = game.Content.Load<SpriteFont>("Font1"); //loads Font1
             back = game.Content.Load<Texture2D>("back");
+            info = new SaveInfo();
+            count = 0;
         }
         #endregion Constructor
 
@@ -45,56 +48,44 @@ namespace FloorIsLava
                 {
                     gameState.CurrentScreen = Screen.StartScreen;
                 }
-                lastState = keyState;
-
-                if (keyState.IsKeyDown(Keys.S) && lastState.IsKeyDown(Keys.S))
-                {
-                    gameState.CurrentScreen = Screen.StartScreen;
-                }
-                if (keyState.IsKeyDown(Keys.G) && lastState.IsKeyDown(Keys.G))
-                {
-                    gameState.StartGame("text.txt");
-                }
-                if (keyState.IsKeyDown(Keys.I) && lastState.IsKeyDown(Keys.I))
-                {
-                    gameState.SwitchInstruct(game);
-                }
-                if (keyState.IsKeyDown(Keys.L) && lastState.IsKeyDown(Keys.L))
-                {
-                    gameState.SwitchLevel(game);
-                }
-                if (keyState.IsKeyDown(Keys.C) && lastState.IsKeyDown(Keys.C))
-                {
-                    gameState.SwitchCredit(game);
-                }
             }
             else //if the pause screen is active only allow player to go back to pause menu
             {
-                if (keyState.IsKeyDown(Keys.Back) && keyState.IsKeyDown(Keys.Back))
+                if (keyState.IsKeyDown(Keys.Back) && lastState.IsKeyUp(Keys.Back))
                 {
                     gameState.SwitchPause(); //switches to pause screen
                 }
-                lastState = keyState;
-
-                if (keyState.IsKeyDown(Keys.S) && lastState.IsKeyDown(Keys.S))
+            }
+            if((keyState.IsKeyDown(Keys.W) && lastState.IsKeyUp(Keys.W)) || (keyState.IsKeyDown(Keys.Up) && lastState.IsKeyUp(Keys.Up)))
+            {
+                count = count - 1;
+                if (count < 0)
                 {
-                    gameState.CurrentScreen = Screen.StartScreen;
+                    count = 2;
                 }
-                if (keyState.IsKeyDown(Keys.G) && lastState.IsKeyDown(Keys.G))
+            }
+            if ((keyState.IsKeyDown(Keys.S) && lastState.IsKeyUp(Keys.S)) || (keyState.IsKeyDown(Keys.Down) && lastState.IsKeyUp(Keys.Down)))
+            {
+                count = count + 1;
+                if (count > 2)
                 {
-                    gameState.StartGame("text.txt");
+                    count = 0;
                 }
-                if (keyState.IsKeyDown(Keys.I) && lastState.IsKeyDown(Keys.I))
+            }
+            if (keyState.IsKeyDown(Keys.Enter) && lastState.IsKeyUp(Keys.Enter))
+            {
+                if (count == 0)
                 {
-                    gameState.SwitchInstruct(game);
+                    //toggle fullscreen code
                 }
-                if (keyState.IsKeyDown(Keys.L) && lastState.IsKeyDown(Keys.L))
+                if (count == 1)
                 {
-                    gameState.SwitchLevel(game);
+                    //reset highscore code 
                 }
-                if (keyState.IsKeyDown(Keys.C) && lastState.IsKeyDown(Keys.C))
+                if (count == 2)
                 {
-                    gameState.SwitchCredit(game);
+                    //reset level lock code goes here
+                    info.ResetLevelLock();
                 }
             }
             lastState = keyState; //assigns current keyboard state to lastState
@@ -108,7 +99,30 @@ namespace FloorIsLava
             spriteBatch.Draw(title, new Rectangle(game.screenWidth / 2 - 500, 100, 1000, 512), Color.White);
             //spriteBatch.DrawString(font1, "Press \"Back\" to go back", new Vector2(50f, 70f), Color.Blue);
             spriteBatch.Draw(back, new Rectangle(50, 60, 150, 80), Color.White);
-            spriteBatch.DrawString(font1, "COMING SOON TO A UPDATE NEAR YOU", new Vector2(game.screenWidth / 2 - 200, 300f), Color.Gold);
+            if (count == 0)
+            {
+                spriteBatch.DrawString(font1, "FullScreen: On", new Vector2(game.screenWidth / 2 - 100, 400f), Color.Crimson);
+            }
+            else
+            {
+                spriteBatch.DrawString(font1, "FullScreen: On", new Vector2(game.screenWidth / 2 - 100, 400f), Color.White);
+            }
+            if (count == 1)
+            {
+                spriteBatch.DrawString(font1, "Reset Level HighScores", new Vector2(game.screenWidth / 2 - 100, 500f), Color.Crimson);
+            }
+            else
+            {
+                spriteBatch.DrawString(font1, "Reset Level HighScores", new Vector2(game.screenWidth / 2 - 100, 500f), Color.White);
+            }
+            if (count == 2)
+            {
+                spriteBatch.DrawString(font1, "Reset Level Lock Status", new Vector2(game.screenWidth / 2 - 100, 600f), Color.Crimson);
+            }
+            else
+            {
+                spriteBatch.DrawString(font1, "Reset Level Lock Status", new Vector2(game.screenWidth / 2 - 100, 600f), Color.White);
+            }
         }
         #endregion Methods
     }
